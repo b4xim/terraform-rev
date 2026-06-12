8d8317fe9face823aa045c5629a414ce6a9c7574
B4xim <bk6549@srmist.edu.in>
2026-06-12 14:54:00 +0530

    Added dynamoDB locking

--- providers.tf
+++ providers.tf
@@ -2,9 +2,16 @@ terraform{
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 5.0"
+            version = "~> 6.0"
         }
     }
+    backend "s3" {
+    bucket         = "bladeoski-tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tfstate-blade"
+    encrypt        = true
+  }
 }
 provider "aws" {
     region = var.aws_region
