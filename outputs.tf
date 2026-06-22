bf53a25dbc44d6d62524d4e8c88ce1f0801eacc1
B4xim <bk6549@srmist.edu.in>
2026-06-22 23:51:20 +0530

    output.tf corrected

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
   output "ip_addr-1" {
-    value = aws_instance.blade-ec2[0].public_ip
+    value = aws_instance.blade-ec2["blade-ec2-1"].public_ip
   }
   output "ip_addr-2" {
-    value = aws_instance.blade-ec2[1].public_ip
+    value = aws_instance.blade-ec2["blade-ec2-2"].public_ip
   }
   
